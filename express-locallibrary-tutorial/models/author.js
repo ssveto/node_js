727af65383aa9848498c57961fa516c881b2c020
ssveto <96317816+ssveto@users.noreply.github.com>
2024-03-07 13:59:28 +0100

    created read only part of locallibrary

--- express-locallibrary-tutorial/models/author.js
+++ express-locallibrary-tutorial/models/author.js
@@ -35,4 +35,8 @@ AuthorSchema.virtual("date_of_death_formatted").get(function () {
 
 });
 
+AuthorSchema.virtual("lifespan").get(function() {
+    return `${this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : ""} - ${this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : ""}`;
+})
+
 module.exports = mongoose.model("Author", AuthorSchema);
