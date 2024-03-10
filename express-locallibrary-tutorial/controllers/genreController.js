4740c30c1a109d462218780beddc057a9049677d
ssveto <96317816+ssveto@users.noreply.github.com>
2024-03-10 23:56:28 +0100

    delete genre and author

--- express-locallibrary-tutorial/controllers/genreController.js
+++ express-locallibrary-tutorial/controllers/genreController.js
@@ -75,12 +75,42 @@ exports.genre_create_post = [
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete GET");
+
+  const [genre, allBooksByGenre] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Book.find({ genre: req.params.id }, "title summary").exec(),
+  ])
+
+  if (genre === null) {
+    res.redirect("/catalog/genres")
+
+  }
+
+  res.render("genre_delete", {
+    title: "Delete Genre",
+    genre_books: allBooksByGenre,
+    genre: genre,
+  })
 });
 
 // Handle Genre delete on POST.
 exports.genre_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete POST");
+  const [genre, allBooksByGenre] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Book.find({ genre: req.params.id }, "title summary").exec(),
+  ])
+
+  if (allBooksByGenre.length > 0) {
+    res.render("genre_delete", {
+      title: "Delete Genre",
+      genre_books: allBooksByGenre,
+      genre: genre,
+    })
+    return;
+  } else {
+    await Genre.findByIdAndDelete(req.body.genreid);
+    res.redirect("/catalog/genres")
+  }
 });
 
 // Display Genre update form on GET.
