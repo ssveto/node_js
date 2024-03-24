60941362923188427a64838aeaab1129718e88bd
ssveto <svetislav.dragovich@gmail.com>
2024-03-24 19:48:17 +0100

    finished CRUD for beautiful_things project

--- beautiful_things/controllers/categoryController.js
+++ beautiful_things/controllers/categoryController.js
@@ -1,6 +1,17 @@
 const Category = require("../models/category");
 const Item = require("../models/item");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+
+
+const isValidUrl = (value) => {
+  try {
+    new URL(value);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
 
 
 exports.index = asyncHandler(async (req, res, next) => {
@@ -59,30 +70,162 @@ exports.category_detail = asyncHandler(async (req, res, next) => {
 
   // Display Category create form on GET.
 exports.category_create_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Author create GET");
+  const [
+    categories,
+    items,
+  ] = await Promise.all([
+    Category.find({}, "name").sort({ name: 1 }).exec(),
+    Item.find().sort({ title: 1 }).exec()
+  ]);
+
+  res.render("category_form", {
+    title: "Create New Category",
+    items: items,
+    categories: categories,
+  })
 });
 
   // Handle Category create on POST.
-exports.category_create_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Author create POST");
-});
+exports.category_create_post = [
+  body("name", "Name must be specified").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must be given").trim().isLength({ min: 10 }).escape(),
+  body("user_name", "The name of the user must be given").trim().isLength({ min: 1 }).escape(),
+  body("url_search", 'Invalid URL format').trim().isURL().isLength({ min: 1 }).custom(isValidUrl),
+  body("url_picture", 'Invalid URL of the picture').trim().isURL().isLength({ min: 1 }).custom(isValidUrl),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({
+      name: req.body.name,
+      category: req.body.category,
+      description: req.body.description,
+      url_search: req.body.url_search,
+      url_picture: req.body.url_picture,
+      created_by: req.body.user_name
+    });
+
+    if (!errors.isEmpty()) {
+
+      const categories = await Category.find().sort({ name: 1 }).exec();
+    
+      res.render("category_form", {
+        title: "Create New Category",
+        category: category,
+        categories: categories,
+        errors: errors.array()
+      })
+
+    } else {
+      await category.save();
+      res.redirect(category.url);
+    }
+  })
+];
 
   // Display Category delete form on GET.
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Author delete GET");
+  const [
+    category,
+    allItems,
+    allCategories
+  ] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Item.find({ category: req.params.id }).sort({ title: 1 }).exec(),
+    Category.find({}, "name").sort({ name: 1 }).exec()
+
+  ]);
+  
+  if (category === null) {
+    res.redirect("/catalog");
+  }
+
+  res.render("category_delete", {
+    title: category.name,
+    items: allItems,
+    category: category,
+    categories: allCategories
+  })
 });
 
   // Handle Category delete on POST.
 exports.category_delete_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Author delete POST");
+  const [
+    category,
+    allItems,
+    allCategories
+  ] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Item.find({ category: req.params.id }).sort({ title: 1 }).exec(),
+    Category.find({}, "name").sort({ name: 1 }).exec()
+
+  ]);
+
+  if (allItems.length > 0) {
+    res.render("category_delete", {
+      title: category.name,
+      items: allItems,
+      category: category,
+      categories: allCategories
+    })
+  } else {
+    await Category.findByIdAndDelete(req.body.categoryid);
+    res.redirect("/catalog/")
+  }
+
 });
 
   // Display Category update form on GET.
 exports.category_update_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Author update GET");
-});
+  const [
+    categories,
+    category
+  ] = await Promise.all([
+    Category.find({}, "name").sort({ name: 1 }).exec(),
+    Category.findById(req.params.id).exec()
+  ]);
+
+  res.render("category_form", {
+    title: "Update Category",
+    category: category,
+    categories: categories,
+  })});
 
   // Handle Category update on POST.
-exports.category_update_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Author update POST");
-});
+exports.category_update_post = [
+  body("name", "Name must be specified").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must be given").trim().isLength({ min: 10 }).escape(),
+  body("user_name", "The name of the user must be given").trim().isLength({ min: 1 }).escape(),
+  body("url_search", 'Invalid URL format').trim().isURL().isLength({ min: 1 }).custom(isValidUrl),
+  body("url_picture", 'Invalid URL of the picture').trim().isURL().isLength({ min: 1 }).custom(isValidUrl),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({
+      name: req.body.name,
+      category: req.body.category,
+      description: req.body.description,
+      url_search: req.body.url_search,
+      url_picture: req.body.url_picture,
+      created_by: req.body.user_name,
+      _id: req.params.id
+    });
+
+    if (!errors.isEmpty()) {
+
+      const categories = await Category.find().sort({ name: 1 }).exec();
+    
+      res.render("category_form", {
+        title: "Update Category",
+        category: category,
+        categories: categories,
+        errors: errors.array()
+      })
+
+    } else {
+      const updatedCategory = await Category.findByIdAndUpdate(req.params.id, category, {});
+      res.redirect(updatedCategory.url);
+    }
+  })
+];
--- beautiful_things/controllers/itemController.js
+++ beautiful_things/controllers/itemController.js
@@ -95,20 +95,84 @@ exports.item_create_post = [
 
   // Display Author delete form on GET.
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Author delete GET");
+  const item = await Item.findById(req.params.id).exec();
+  const categories = await Category.find().sort({ name: 1 }).exec();
+
+
+  if (item === null) {
+    res.redirect("/catalog/")
+  }
+
+  res.render("item_delete", {
+    title: "Delete Item",
+    item: item,
+    categories: categories,
+
+
+  })
 });
 
   // Handle Author delete on POST.
 exports.item_delete_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Author delete POST");
+  await Item.findByIdAndDelete(req.body.itemid);
+  res.redirect("/catalog/")
 });
 
   // Display Author update form on GET.
 exports.item_update_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Author update GET");
+  const [
+    categories,
+    items,
+    item
+  ] = await Promise.all([
+    Category.find({}, "name").sort({ name: 1 }).exec(),
+    Item.find().sort({ title: 1 }).exec(),
+    Item.findById(req.params.id).populate("category").exec()
+  ]);
+
+  res.render("item_form", {
+    title: "Update Item",
+    items: items,
+    item: item,
+    categories: categories,
+  })
 });
 
   // Handle Author update on POST.
-exports.item_update_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Author update POST");
-});
+exports.item_update_post = [
+  body("title", "Title must be specified").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must be given").trim().isLength({ min: 10 }).escape(),
+  body("user_name", "The name of the user must be given").trim().isLength({ min: 1 }).escape(),
+  body("url_search", 'Invalid URL format').trim().isURL().isLength({ min: 1 }).custom(isValidUrl),
+  body("url_picture", 'Invalid URL of the picture').trim().isURL().isLength({ min: 1 }).custom(isValidUrl),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const item = new Item({
+      title: req.body.title,
+      category: req.body.category,
+      description: req.body.description,
+      url_search: req.body.url_search,
+      url_picture: req.body.url_picture,
+      created_by: req.body.user_name,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+
+      const categories = await Category.find().sort({ name: 1 }).exec();
+    
+      res.render("item_form", {
+        title: "Update Item",
+        item: item,
+        categories: categories,
+        errors: errors.array()
+      })
+
+    } else {
+      const updatedItem = await Item.findByIdAndUpdate(req.params.id, item, {});
+      res.redirect(updatedItem.url);
+    }
+  })
+];
