6a83650f41da4122184c1b65695bf050aafe6f55
ssveto <96317816+ssveto@users.noreply.github.com>
2024-05-06 15:30:27 +0200

    added signup form

--- members-only/app.js
+++ members-only/app.js
@@ -1,19 +1,20 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require("mongoose");
+
 require('dotenv').config()
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 app = express();
 
-// mongoDB
-
 // Set up mongoose connection
-const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 const mongoDB = process.env.MONGODB_URI;
 
@@ -23,18 +24,18 @@ async function main() {
   await mongoose.connect(mongoDB);
 }
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- members-only/routes/index.js
+++ members-only/routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
+
+const asyncHandler = require("express-async-handler");
+const member = require("../models/member");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get("/sign-up", (req, res) => res.render('sign-up-form'));
+
+router.post("/sign-up", async (req, res, next) => {
+
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+        if (err) {
+            return next(err);
+        } else {
+            const member = new Member({
+                username: req.body.username,
+                password: hashedPassword
+            });
+            const result = await member.save();
+            res.redirect("/");
+        }
+        
+    });
+
 });
 
 module.exports = router;
